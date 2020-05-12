39a02e885d1a4dd844ad299d45a0ce10aeb7e007
cognitonow <cognito.learning.now@gmail.com>
2020-05-12 12:47:29 +0200

    z
    demo

---
+++ BRA031017/story_content/user.js
@@ -0,0 +1,23 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6mfNUwNndcX":
+        Script1();
+        break;
+      case "5djzhTBVdMP":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  SetStatus("completed")
+}
+
+function Script2()
+{
+  SetStatus("completed")
+}
+
